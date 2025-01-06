1be92151db11a71468c9657e12bd46fb8b40dcd4
Liron <liron.ar@gmail.com>
2025-01-06 13:05:58 +0200

    work with database

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -6,20 +6,29 @@ import il.cshaifasweng.OCSFMediatorExample.server.ocsf.ConnectionToClient;
 import java.io.IOException;
 import java.util.*;
 
-import il.cshaifasweng.OCSFMediatorExample.entities.Warning;
 import il.cshaifasweng.OCSFMediatorExample.server.ocsf.SubscribedClient;
 import il.cshaifasweng.OCSFMediatorExample.entities.*;
 
 import java.util.List;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 
 
 
 public class SimpleServer extends AbstractServer {
 	private static ArrayList<SubscribedClient> SubscribersList = new ArrayList<>();
-	private MenuItemsController menuItemsController = new MenuItemsController();
+
+	private static Session session;
+	private Menu menu=new Menu();
 
 	public SimpleServer(int port) {
 		super(port);}
@@ -27,15 +36,8 @@ public class SimpleServer extends AbstractServer {
 	@Override
 	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
 		String msgString = msg.toString();
-		if (msgString.startsWith("#warning")) {
-			Warning warning = new Warning("Warning from server!");
-			try {
-				client.sendToClient(warning);
-				System.out.format("Sent warning to client %s\n", client.getInetAddress().getHostAddress());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else if (msgString.startsWith("add client")) {
+
+		if (msgString.startsWith("add client")) {
 			SubscribedClient connection = new SubscribedClient(client);
 			SubscribersList.add(connection);
 			try {
@@ -50,16 +52,11 @@ public class SimpleServer extends AbstractServer {
 
 		else if (msgString.startsWith("#display menu"))
 		{
-			System.out.println("Displaying menu");
-			MenuItem item2 = new MenuItem(
-					"Pizza ",
-					55.00,
-					" Mushrooms, onions, tomatoes ",
-					"Includes vegan option ",
-					null
-			);
-			List<MenuItem> menuList = new ArrayList<>(List.of(item2));
-			Menu menu=new Menu(menuList);
+
+
+			displayMenuFun();
+
+
 
 			try {
 				client.sendToClient(menu);//sent the menu
@@ -72,37 +69,7 @@ public class SimpleServer extends AbstractServer {
 
 
 
-	private void updateItemInDB(MenuItem item) {
-		Session session = null;
-		try {
-			// Initialize the Hibernate session
-			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-			session = sessionFactory.openSession();
-			session.beginTransaction();
 
-			// Double the price of the item
-			double currentPrice = item.getPrice();
-			double newPrice = currentPrice * 2;
-			item.setPrice(newPrice);
-
-			// Update the item in the database
-			session.merge(item);
-
-			// Commit the transaction
-			session.getTransaction().commit();
-
-			System.out.println("Item with ID " + item.getItemID() + " updated successfully. New price: " + newPrice);
-		} catch (Exception e) {
-			if (session != null) {
-				session.getTransaction().rollback();
-			}
-			e.printStackTrace();
-		} finally {
-			if (session != null) {
-				session.close();
-			}
-		}
-	}
 
 
 
@@ -115,7 +82,118 @@ public class SimpleServer extends AbstractServer {
 			e1.printStackTrace();
 		}
 	}
-	private void updateDish(int ItemId, int price) {
-		System.out.println("in updateDish");
+
+
+	private void displayMenuFun() {
+		try {
+			System.out.println("indisp!!!!!!!!!!");
+
+			SessionFactory sessionFactory = getSessionFactory(); //need to define session factory
+			session = sessionFactory.openSession(); // have to do next two lines to make actions in the DB
+			session.beginTransaction();
+//after begin transaction we can make actions in the database, when we finish we make commit
+			System.out.println("Displaying menu");
+			initializeData();
+
+			menu.SetMenuItems(getMenuItems());
+
+
+			session.getTransaction().commit(); // Save everything.
+
+		} catch (Exception exception) {
+			if (session != null) {
+				session.getTransaction().rollback();
+			}
+			System.err.println("An error occured, changes have been rolled back.");
+			exception.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+	private List<MenuItem> getMenuItems() {
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<MenuItem> query = builder.createQuery(MenuItem.class);
+		query.from(MenuItem.class);
+		List<MenuItem> data = session.createQuery(query).getResultList();
+		return data;
+	}
+
+	private static SessionFactory getSessionFactory() throws
+			HibernateException {
+
+		Configuration configuration = new Configuration();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter the database password: ");
+		String password = scanner.nextLine();
+		configuration.setProperty("hibernate.connection.password", password);
+//add dynamic password here
+		// Add ALL of your entities(Classes) here. You can also try adding a whole package.
+		configuration.addAnnotatedClass(Menu.class);
+		configuration.addAnnotatedClass(MenuItem.class);
+
+
+		ServiceRegistry serviceRegistry = new
+				StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties())
+				.build();
+
+		return configuration.buildSessionFactory(serviceRegistry);
+	}
+
+
+	public static void initializeData() {
+
+		// יצירת פריטים עבור התפריט בעזרת הבנאי
+		MenuItem item1 = new MenuItem(
+				"Salad",
+				35.00,
+				"Tomatoes, cucumbers, lettuce",
+				"Low calorie",
+				null
+		);
+
+		MenuItem item2 = new MenuItem(
+				"Pizza ",
+				55.00,
+				" Mushrooms, onions, tomatoes",
+				" Includes vegan option ",
+				null
+		);
+
+		MenuItem item3 = new MenuItem(
+				"Pasta",
+				60.00,
+				"Mushroom cream sauce",
+				"Available gluten-free",
+				null
+		);
+
+		MenuItem item4 = new MenuItem(
+				"Hamburger",
+				80.00,
+				"Meatball, pickle, tomato, lettuce",
+				"Choice of meat or plant-based",
+				null
+		);
+
+		MenuItem item5 = new MenuItem(
+				"Edamame",
+				30.00,
+				"Edamame",
+				"Served with sea salt",
+				null
+		);
+
+		// שמירת הפריטים במסד הנתונים
+		session.save(item1);
+		session.save(item2);
+		session.save(item3);
+		session.save(item4);
+		session.save(item5);
+
+		System.out.println("Data initialization completed!");
+		session.flush();
+
 	}
 }
+
