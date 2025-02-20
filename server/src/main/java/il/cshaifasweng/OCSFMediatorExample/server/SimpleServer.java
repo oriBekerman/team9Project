7507d90cfb5403eff5b6e0480190faee6ad1c657
oriBekerman <orib26@gmail.com>
2025-02-20 11:37:54 +0200

    change request - response

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -49,33 +49,6 @@ public class SimpleServer extends AbstractServer {
         System.out.println("received request from client: ");
         String msgString = msg.toString();
         Request request=(Request)msg;
-        if(request.getCategory()==BRANCH)
-        {
-            Response response=branchController.handleRequest(request);
-            try {
-                client.sendToClient(response);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        if(request.getCategory()==BASE_MENU)
-        {
-            System.out.println("server got menu request");
-            Response response=menuItemsController.handleRequest(request);
-            try {
-                if(response.getRecipient()==ALL_CLIENTS)
-                {
-                    sendToAllClients(response);
-                    System.out.println("server SENT TO all clients");
-                }
-                if (response.getRecipient()==THIS_CLIENT)
-                {
-                    client.sendToClient(response);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
         if (msgString.startsWith("add client")) {
             SubscribedClient connection = new SubscribedClient(client);
             SubscribersList.add(connection);
@@ -87,12 +60,6 @@ public class SimpleServer extends AbstractServer {
             }
 
         }
-        //receives edit item msg from client and returns the updated item
-//        else if(request.getRequestType().equals(UPDATE_PRICE)) {
-//            MenuItem item = menuItemsController.updatePrice(request);
-//            Response<MenuItem> response = new Response<>(UPDATED_PRICE, item, SUCCESS);
-//            sendToAllClients((response));//sent the item to all the clients
-//        }
         else if (request.getRequestType().equals(CHECK_USER)) {
             try {
                 String data = (String) request.getData();
@@ -123,8 +90,96 @@ public class SimpleServer extends AbstractServer {
                 e.printStackTrace();
             }
         }
+        Response response = switch (request.getCategory())
+        {
+            case BASE_MENU -> menuItemsController.handleRequest(request);
+            case BRANCH -> branchController.handleRequest(request);
+            default -> throw new IllegalArgumentException("Unknown request category: " + request.getCategory());
+        };
+        if (response.getRecipient()==ALL_CLIENTS) {
+            sendToAllClients(response);
+        }
+        if (response.getRecipient()==THIS_CLIENT) {
+            try {
+                client.sendToClient(response);
+            } catch (Exception e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+//        if(request.getCategory()==BRANCH)
+//        {
+//            Response response=branchController.handleRequest(request);
+//            try {
+//                client.sendToClient(response);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//        if(request.getCategory()==BASE_MENU)
+//        {
+//            System.out.println("server got menu request");
+//            Response response=menuItemsController.handleRequest(request);
+//            try {
+//                if(response.getRecipient()==ALL_CLIENTS)
+//                {
+//                    sendToAllClients(response);
+//                    System.out.println("server SENT TO all clients");
+//                }
+//                if (response.getRecipient()==THIS_CLIENT)
+//                {
+//                    client.sendToClient(response);
+//                }
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//        if (msgString.startsWith("add client")) {
+//            SubscribedClient connection = new SubscribedClient(client);
+//            SubscribersList.add(connection);
+//            try {
+//                client.sendToClient("client added successfully");
+//                System.out.println("Client added successfully");
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        }
+//        else if (request.getRequestType().equals(CHECK_USER)) {
+//            try {
+//                String data = (String) request.getData();
+//                String[] credentials = data.split(" ");
+//                String userName = credentials[0];
+//                String password = credentials[1];
+//
+//                System.out.println("Checking login for: " + userName);
+//
+//                String loginResult = logInController.verifyUser(userName, password);
+//                // Debug prints after verifyUser()
+//                System.out.println("Login result from controller: " + loginResult);
+//
+//                Response<String> response;
+//                if (loginResult.equals("Login successful")) {
+//                    EmployeeType employeeType = logInController.getEmployeeTypeByUsername(userName);
+//                    response = new Response<>(CORRECTNESS_USER, userName + ":" + employeeType , SUCCESS,THIS_CLIENT);
+//                } else {
+//                    response = new Response<>(CORRECTNESS_USER, loginResult,ERROR,THIS_CLIENT);
+//                }
+//
+//                System.out.println("Preparing to send response with message: " + response.getMessage());
+//                client.sendToClient(response);
+//                System.out.println("Response sent successfully.");
+//
+//            } catch (Exception e) {
+//                System.err.println("Exception in CHECK_USER handling: " + e.getMessage());
+//                e.printStackTrace();
+//            }
+//        }
 
     }
+
+
+
     public void sendToAllClients(String message) {
         try {
             for (SubscribedClient subscribedClient : SubscribersList) {
