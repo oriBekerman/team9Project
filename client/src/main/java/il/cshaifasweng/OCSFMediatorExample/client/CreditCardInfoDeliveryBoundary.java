29c405f92db3e79bf56e27614b39da8b91b5340a
galAlpert <galalpert5@gmail.com>
2025-03-20 21:21:35 +0200

    fixed files for delivery

---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/CreditCardInfoDeliveryBoundary.java
@@ -0,0 +1,161 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import java.net.URL;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+
+import il.cshaifasweng.OCSFMediatorExample.client.Events.CreditCardInfoSet;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.greenrobot.eventbus.EventBus;
+
+import static il.cshaifasweng.OCSFMediatorExample.client.App.switchScreen;
+
+public class CreditCardInfoDeliveryBoundary {
+
+
+    public TextField cardNumText;
+    public TextField expDateText;
+    public TextField cvvText;
+    public Label errorLabel;
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Button paymentBtn;
+    private String type;
+    public boolean typeIsSet=false;
+
+    public CreditCardInfoDeliveryBoundary() {}
+
+    public CreditCardInfoDeliveryBoundary(String type) {
+        this.type = type;
+        this.typeIsSet=true;
+    }
+
+    @FXML
+    void backToPersonalD(ActionEvent event) {
+        switchScreen("Personal Details Filling");
+    }
+
+    @FXML
+    void checkPayment(ActionEvent event) {
+        String cardNum = cardNumText.getText();
+        String expDate = expDateText.getText();
+        String cvv = cvvText.getText();
+        if(cardNum.isEmpty() || expDate.isEmpty() || cvv.isEmpty()) {
+            errorLabel.setText("Please fill all the fields");
+        }
+        else if(!isValidCreditCard(cardNum))
+        {
+            errorLabel.setText("Invalid Card Number");
+        }
+        else if(!isValidExpDate(expDate))
+        {
+            errorLabel.setText("Invalid Expiry Date");
+        }
+        else if (!isValidCVV(cvv))
+        {
+            errorLabel.setText("Invalid CVV");
+        }
+        else {
+            if(type=="reservation")
+            {
+                SimpleClient.getClient().mapReservation.put("cardNum", cardNum);
+                SimpleClient.getClient().mapReservation.put("expDate", expDate);
+                SimpleClient.getClient().mapReservation.put("cvv", cvv);
+
+                // stop reservation timer since pay is pressed and reservation will be saved
+                TimerManager.getInstance().cancelTimer("reservationTimeout");
+
+                CreditCardInfoSet events = new CreditCardInfoSet();
+                EventBus.getDefault().post(events);
+            }
+
+        }
+
+    }
+
+    @FXML
+    void initialize() {
+        assert backBtn != null : "fx:id=\"backBtn\" was not injected: check your FXML file 'creditCardInfo.fxml'.";
+        assert paymentBtn != null : "fx:id=\"paymentBtn\" was not injected: check your FXML file 'creditCardInfo.fxml'.";
+
+
+    }
+    public void setType(String type) {
+        System.out.println("in set type before sync");
+        synchronized (this) {
+            if (this.type == null || !this.type.equals(type)) {
+                System.out.println("in type map after sync");
+                this.type = type;
+                this.typeIsSet = true;
+                notifyAll();
+            }
+        }
+    }
+
+
+
+    //validate card number
+    public boolean isValidCreditCard(String cardNumber) {
+        if (!Pattern.compile("^[0-9]{13,19}$").matcher(cardNumber).matches()) {
+            return false;
+        }
+        return luhnCheck(cardNumber);
+    }
+    //used to validate card number
+    private boolean luhnCheck(String cardNumber) {
+        int sum = 0;
+        boolean alternate = false;
+        for (int i = cardNumber.length() - 1; i >= 0; i--) {
+            int digit = Character.getNumericValue(cardNumber.charAt(i));
+            if (alternate) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+            sum += digit;
+            alternate = !alternate;
+        }
+        return sum % 10 == 0;
+    }
+    public static boolean isValidCVV(String cvv) {
+        return Pattern.compile("^[0-9]{3,4}$").matcher(cvv).matches();
+    }
+
+    //check expiration date is a valid date
+    public static boolean isValidExpDate(String expDate) {
+        if (!Pattern.compile("^(0[1-9]|1[0-2])/(\\d{2}|\\d{4})$").matcher(expDate).matches()) {
+            return false;
+        }
+        return isNotExpired(expDate);
+    }
+    //check exp date has not passed
+    private static boolean isNotExpired(String expDate) {
+        try {
+            DateTimeFormatter formatter = expDate.length() == 5 ? DateTimeFormatter.ofPattern("MM/yy")
+                    : DateTimeFormatter.ofPattern("MM/yyyy");
+            YearMonth cardExpiry = YearMonth.parse(expDate, formatter);
+            return cardExpiry.isAfter(YearMonth.now()); // Expiration must be in the future
+        } catch (
+                DateTimeParseException e) {
+            return false;
+        }
+    }
+
+
+}
---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/PersonalDetailsFillingDeliveryBoundary.java
@@ -0,0 +1,149 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import il.cshaifasweng.OCSFMediatorExample.client.Events.ComplaintCustomerEvent;
+import il.cshaifasweng.OCSFMediatorExample.client.Events.ReservationPersonalInfoSet;
+import il.cshaifasweng.OCSFMediatorExample.entities.Branch;
+import il.cshaifasweng.OCSFMediatorExample.entities.Request;
+import il.cshaifasweng.OCSFMediatorExample.entities.RestTable;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.greenrobot.eventbus.EventBus;
+
+import static il.cshaifasweng.OCSFMediatorExample.client.App.switchScreen;
+import static il.cshaifasweng.OCSFMediatorExample.entities.ReqCategory.BRANCH;
+import static il.cshaifasweng.OCSFMediatorExample.entities.RequestType.UPDATE_BRANCH;
+
+public class PersonalDetailsFillingDeliveryBoundary{
+    public Label errorLabel;
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Button contToCCInfoBtn;
+
+    @FXML
+    private TextField mailTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField phoneTextField;
+
+    private String type;
+    public boolean typeIsSet=false;
+
+
+    public PersonalDetailsFillingDeliveryBoundary(String type) {
+        this.type = type;
+        this.typeIsSet=true;
+    }
+
+    public PersonalDetailsFillingDeliveryBoundary() {}
+    @FXML
+    void contToCCinfoFill(ActionEvent event) {
+        String name=nameTextField.getText();
+        String phone=phoneTextField.getText();
+        String mail=mailTextField.getText();
+        if(name.isEmpty() || phone.isEmpty() || mail.isEmpty())
+        {
+            errorLabel.setText("Please enter all the fields.");
+        }
+        else if(!isValidPhone(phone))
+        {
+            errorLabel.setText("Please enter a valid phone number.");
+        }
+        else if (!isValidEmail(mail))
+        {
+            errorLabel.setText("Please enter a valid email address.");
+        }
+        else if(type =="reservation")
+        {
+            SimpleClient.getClient().mapReservation.put("name",name);
+            SimpleClient.getClient().mapReservation.put("phone",phone);
+            SimpleClient.getClient().mapReservation.put("mail",mail);
+            openCreditCardPage();
+        }
+    }
+
+    @FXML
+    void backToReservation(ActionEvent event) {
+
+    }
+    @FXML
+    void initialize() {
+        assert backBtn != null : "fx:id=\"backBtn\" was not injected: check your FXML file 'personalDetailsFilling.fxml'.";
+        assert contToCCInfoBtn != null : "fx:id=\"contToCCinfoBtn\" was not injected: check your FXML file 'personalDetailsFilling.fxml'.";
+        assert mailTextField != null : "fx:id=\"mailTextField\" was not injected: check your FXML file 'personalDetailsFilling.fxml'.";
+        assert nameTextField != null : "fx:id=\"nameTextField\" was not injected: check your FXML file 'personalDetailsFilling.fxml'.";
+        assert phoneTextField != null : "fx:id=\"phoneTextField\" was not injected: check your FXML file 'personalDetailsFilling.fxml'.";
+
+
+    }
+    // initialize the map before letting the map page be opened
+    public void setType(String type) {
+        System.out.println("in set type before sync");
+        synchronized (this) {
+            if (this.type == null || !this.type.equals(type)) {
+                System.out.println("in type map after sync");
+                this.type = type;
+                this.typeIsSet = true;
+                notifyAll();
+            }
+        }
+    }
+
+    public static boolean isValidPhone(String phone) {
+        return Pattern.compile("^(\\+\\d{1,3})?\\d{10,15}$").matcher(phone).matches();
+    }
+
+    public static boolean isValidEmail(String email) {
+        return Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$").matcher(email).matches();
+    }
+    public void openCreditCardPage() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("creditCardInfo.fxml"));
+            Parent creditCardInfoPageRoot = loader.load();
+            // Get the controller and set the type before waiting
+            CreditCradInfoBoundary boundary = loader.getController();
+            boundary.setType("reservation");  // This should be set before waiting
+            synchronized (boundary) {
+                while (!boundary.typeIsSet) {
+                    System.out.println("Waiting for type to be set...");
+                    boundary.wait();  // Waits until notifyAll() is called
+                }
+            }
+            Platform.runLater(() -> {
+                try {
+                    App.setContent(creditCardInfoPageRoot);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();  // Restore interrupted state
+        }
+    }
+
+}
